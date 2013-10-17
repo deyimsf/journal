ade4713fe160249d1aba7f4402d3593abebd135b
mason <deyimsf@163.com>
2013-10-17 02:52:39 -0700

    demo init

---
+++ C/demo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_COLS 20
+#define MAX_INPUT 1000
+
+/*
+ 枚举 enum Jar_Type{CUP,PINT,QUART,HALF_GALLON,GALLON}; 
+ */
+
+int main(){
+	/*字符串*/
+	while( gets(input) != NULL){
+		printf("字符串: %s\n",input);
+	}
+}
