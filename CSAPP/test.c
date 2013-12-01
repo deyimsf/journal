144be333b1a02f2314aaa0a967089f48ca0b0a4f
Full Name <root@masf.(none)>
2013-12-01 22:06:37 +0800

    指针

--- CSAPP/test.c
+++ CSAPP/test.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
-void cc(int*,int*);
 main(){
-	int x=2;
-	int *y = &x;
+	int  x=2;
+	int* y = &x; //y是一个整型指针(地址),&x是一个整型指针(地址) *y是y这个地址所指向的内容，是个整型.
 
-	printf("x: %x\n",x);
-	printf("*y: %x\n",*y);printf("*y: %x\n",*y);
+	printf("%d\n",y);     //表示地址
+	printf("%d\n",&x);    //表示地址
+	printf("%d\n",&(*y)); //表示*y这个整型的地址
 
-	printf("y: %x\n",y);
-	printf("++y: %x\n",++y);
-	printf("*y: %x\n",*y);
-
+	printf("%d\n",*y);    //内存地址空间的内容
+	printf("%d\n",*(&x)); //内存地址空间的内容
 }
 
 
