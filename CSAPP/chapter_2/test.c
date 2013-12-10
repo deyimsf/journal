39ecd6e6e556bcc585182d892594d313154950da
mason <deyimsf@163.com>
2013-12-10 02:38:11 -0800

    补码

---
+++ CSAPP/chapter_2/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main(int argc, char *argv[]){
+	
+	char v = -127;
+	unsigned char uv = (unsigned char)v;
+	
+	printf("%i\n", v);
+	printf("%u\n", uv);
+	
+	printf("%x\n", -1<0);
+
+}
