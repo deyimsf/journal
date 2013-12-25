3a670488bde6e43c5ac6c585e1a15bd6cde5faff
mason <deyimsf@163.com>
2013-12-25 03:50:39 -0800

    指针

--- CSAPP/test.c
+++ CSAPP/test.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int fun(int x, int *p){
+	return 2;
+}
 
 main(){
-	int  x=2;
-	int* y = &x; //y是一个整型指针(地址),&x是一个整型指针(地址) *y是y这个地址所指向的内容，是个整型.
+	int (*fp)(int x,int *p);
+	fp = fun;	
 
-	printf("%d\n",y);     //表示地址
-	printf("%d\n",&x);    //表示地址
-	printf("%d\n",&(*y)); //表示*y这个整型的地址
-
-	printf("%d\n",*y);    //内存地址空间的内容
-	printf("%d\n",*(&x)); //内存地址空间的内容
+	printf("%x\n",fp);
+	printf("%x\n",fp+1);
 }
 
 
