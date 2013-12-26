cc203ddbd2f7bfac81d80a4c17c2948b34432eb6
mason <deyimsf@163.com>
2013-12-26 03:33:58 -0800

    update

--- CSAPP/test.c
+++ CSAPP/test.c
@@ -6,11 +6,27 @@ int fun(int x, int *p){
 }
 
 main(){
-	int (*fp)(int x,int *p);
+	/*int (*fp)(int x,int *p);
 	fp = fun;	
 
 	printf("%x\n",fp);
 	printf("%x\n",fp+1);
+
+	struct S1{
+		int i;
+		int j;
+		double a;
+		char c;
+	};
+
+	printf("%d\n",sizeof(struct S1));
+	*/
+
+	int local;
+	printf("local at %p\n",&local);
+	printf("local at %p\n",&local+1);
+	return 0;
+	
 }
 
 
