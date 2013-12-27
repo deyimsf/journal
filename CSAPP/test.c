8933a23f5b499dc7c2b0558cee0e81184112e0b0
mason <deyimsf@163.com>
2013-12-26 23:54:44 -0800

    x86-64对齐

--- CSAPP/test.c
+++ CSAPP/test.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int fun(int x, int *p){
 	return 2;
 }
 
 main(){
-	/*int (*fp)(int x,int *p);
+	int (*fp)(int x,int *p);
 	fp = fun;	
-
 	printf("%x\n",fp);
-	printf("%x\n",fp+1);
+	printf("%x\n\n",fp+1);
+
 
 	struct S1{
 		int i;
@@ -18,10 +17,9 @@ main(){
 		double a;
 		char c;
 	};
-
-	printf("%d\n",sizeof(struct S1));
-	*/
-
+	printf("struct S1 size:%d\n\n",sizeof(struct S1));
+	
+	
 	int local;
 	printf("local at %p\n",&local);
 	printf("local at %p\n",&local+1);
