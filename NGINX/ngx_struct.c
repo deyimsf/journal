4fb7ac522bf1a8c0caa2258250e8968a4b72cc24
masf <deyimsf@163.com>
2015-06-06 20:19:39 +0800

    add ngx_http_module_t

--- NGINX/ngx_struct.c
+++ NGINX/ngx_struct.c
@@ -184,5 +184,18 @@ typedef struct {
 
  }
 
+//自定义模块上下文
+ typedef struct {
+    ngx_int_t   (*preconfiguration)(ngx_conf_t *cf);
+    ngx_int_t   (*postconfiguration)(ngx_conf_t *cf);
 
+    void       *(*create_main_conf)(ngx_conf_t *cf);
+    char       *(*init_main_conf)(ngx_conf_t *cf, void *conf);
+
+    void       *(*create_srv_conf)(ngx_conf_t *cf);
+    char       *(*merge_srv_conf)(ngx_conf_t *cf, void *prev, void *conf);
+
+    void       *(*create_loc_conf)(ngx_conf_t *cf);
+    char       *(*merge_loc_conf)(ngx_conf_t *cf, void *prev, void *conf);
+ } ngx_http_module_t;
 
