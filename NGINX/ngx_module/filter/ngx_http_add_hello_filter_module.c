dcd6bc9553620daafe345cc8341da557114122ff
masf <deyimsf@163.com>
2015-06-14 18:30:57 +0800

    fix folder and add fillter module

--- NGINX/ngx_module/filter/ngx_http_add_hello_filter_module.c
+++ NGINX/ngx_module/filter/ngx_http_add_hello_filter_module.c
@@ -4,11 +4,17 @@
 #include <ngx_core.h>
 #include <ngx_http.h>
 
+static ngx_int_t ngx_http_add_hello_init(ngx_conf_t *cf);
+static void *ngx_http_add_hello_create_loc_conf(ngx_conf_t *cf);
+static char *ngx_http_add_hello_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child);
+static ngx_int_t ngx_http_add_hello_header_filter(ngx_http_request_t *r);
+static ngx_int_t ngx_http_add_hello_body_filter(ngx_http_request_t *r, ngx_chain_t *in);
 
-//模块保存模块指令结构
+//保存模块指令结构
 typedef struct {
 	ngx_str_t	add_hello;
 	
+	//?这俩指令有啥用
 	ngx_hash_t	types;
 	ngx_array_t	*types_keys;
 } ngx_http_add_hello_loc_conf_t;
@@ -29,10 +35,10 @@ static ngx_command_t ngx_http_add_hello_commands[]={
 	},
 
 	ngx_null_command
-}
+};
 
 //上下文
-static ngx_http_module_t ngx_http_add_hello_filter_module_ctx = {
+static ngx_http_module_t ngx_http_add_hello_filter_module_ctx={
 	NULL,
 	ngx_http_add_hello_init,
 
@@ -42,14 +48,14 @@ static ngx_http_module_t ngx_http_add_hello_filter_module_ctx = {
 	NULL,
 	NULL,
 
-	ngx_http_add_hello_create_conf,
-	ngx_http_add_hello_merge_conf
-}
+	ngx_http_add_hello_create_loc_conf,
+	ngx_http_add_hello_merge_loc_conf
+};
 
 //模块信息
 ngx_module_t ngx_http_add_hello_filter_module = {
 	NGX_MODULE_V1,
-	$ngx_http_add_hello_filter_module_ctx,
+	&ngx_http_add_hello_filter_module_ctx,
 	ngx_http_add_hello_commands,
 	NGX_HTTP_MODULE,
 	NULL,
@@ -60,7 +66,7 @@ ngx_module_t ngx_http_add_hello_filter_module = {
 	NULL,
 	NULL,
 	NGX_MODULE_V1_PADDING
-}
+};
 
 //全局变量,过滤头的方法和过滤体的方法
 static ngx_http_output_header_filter_pt	ngx_http_next_header_filter;
@@ -75,15 +81,17 @@ ngx_http_add_hello_init(ngx_conf_t *cf)
 
 	ngx_http_next_body_filter = ngx_http_top_body_filter;
 	ngx_http_top_body_filter = ngx_http_add_hello_body_filter;	
+	
+	return NGX_OK;
 }
 
 //创建本地变量
 static void *
-ngx_http_add_hello_create_conf(ngx_conf_t *cf)
+ngx_http_add_hello_create_loc_conf(ngx_conf_t *cf)
 {
-	ngx_http_add_hello_conf_t *conf;	
+	ngx_http_add_hello_loc_conf_t *conf;	
 	
-	conf = ngx_pcalloc(cf->pool,sizeof(ngx_http_add_hello_conf_t));
+	conf = ngx_pcalloc(cf->pool,sizeof(ngx_http_add_hello_loc_conf_t));
     if(conf == NULL){
 		return NULL;	
 	}	
@@ -93,10 +101,10 @@ ngx_http_add_hello_create_conf(ngx_conf_t *cf)
 
 //merge指令值
 static char *
-ngx_http_add_hello_merge_conf(ngx_conf_t *cf, void *parent, void *child)
+ngx_http_add_hello_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
 {
 	ngx_http_add_hello_loc_conf_t *prev = parent;
-	ngx_http_add_hello_loc_conf_t *next = child;
+	ngx_http_add_hello_loc_conf_t *conf = child;
 
     ngx_conf_merge_str_value(conf->add_hello, prev->add_hello, " defalut");	
 	
@@ -108,34 +116,47 @@ static ngx_int_t
 ngx_http_add_hello_header_filter(ngx_http_request_t *r)
 {
 	ngx_http_add_hello_ctx_t *ctx;
-	ngx_http_add_hello_conf_t *conf;
+	ngx_http_add_hello_loc_conf_t *conf;
+
+	//如果响应状态码不是200,或者不是主请求,就不过滤响应头了
+	if (r->headers_out.status != NGX_HTTP_OK || r != r->main){
+	    return ngx_http_next_header_filter(r);
+	}
 
 	//拿出loc_conf
 	conf = ngx_http_get_module_loc_conf(r, ngx_http_add_hello_filter_module);
 	
-	if(conf->add_hello == NULL){
+	if(conf->add_hello.len == 0){
 		return ngx_http_next_header_filter(r);	
 	}			
 
+	ctx = ngx_pcalloc(r->pool, sizeof(ngx_http_add_hello_ctx_t));
 	ngx_http_set_ctx(r, ctx, ngx_http_add_hello_filter_module);	
 	
-	ngx_http_clear_content_length(r); //?后面谁去指定长度
+	//如果无法响应内容长度则清除content_length头,nginx后续使用chunked编码
+	ngx_http_clear_content_length(r);
 	ngx_http_clear_accept_ranges(r); //?干什么的
 	
     return ngx_http_next_header_filter(r);
 }
 
-//过滤头
+//过滤体
 static ngx_int_t
 ngx_http_add_hello_body_filter(ngx_http_request_t *r, ngx_chain_t *in)
 {
-	ngx_buf_t		*buf;
-	ngx_chain_t		*nct;
-	ngx_http_add_hello_ctx_t *ctx;
+	ngx_buf_t			*buf;
+	ngx_chain_t			*nct;
+	ngx_http_add_hello_ctx_t 	*ctx;
+	ngx_http_add_hello_loc_conf_t 	*conf;
+
+	//如果要输出的链是NULL,或者客户端只要求输出响应头,则不在执行过滤逻辑
+	if (in == NULL || r->header_only) {
+	    return ngx_http_next_body_filter(r, in);
+	}
 
 	//获取在header中保存的ctx
 	ctx = ngx_http_get_module_ctx(r,ngx_http_add_hello_filter_module);
-    if(ctx == NULL){ //如果header没有设置
+   	if(ctx == NULL){ //如果header没有设置
 		return ngx_http_next_body_filter(r,in);
 	}	
 	
@@ -149,10 +170,16 @@ ngx_http_add_hello_body_filter(ngx_http_request_t *r, ngx_chain_t *in)
 	if(nct == NULL) {
 		return NGX_ERROR;		
 	}
-		
-    //为当前filter设置buf
-	buf->pos = ctx->add_hello.data;
-	buf->last = buf->pos + ctx->add_hello.len;
+	
+	conf = ngx_http_get_module_loc_conf(r,ngx_http_add_hello_filter_module);	
+    	
+	//为当前filter设置buf
+	//增加一个换行
+	u_char* tmp_buf = ngx_pcalloc(r->pool, conf->add_hello.len + 5);
+	ngx_sprintf(tmp_buf,"%s\n",conf->add_hello.data);
+	
+	buf->pos = tmp_buf;	//conf->add_hello.data;
+	buf->last = buf->pos + ngx_strlen(tmp_buf); //conf->add_hello.len;
 	buf->start = buf->pos;
 	buf->end = buf->last;
 	buf->last_buf = 1;
@@ -168,7 +195,7 @@ ngx_http_add_hello_body_filter(ngx_http_request_t *r, ngx_chain_t *in)
 		}
 	}
 	
-	tmp.next = nct;
+	tmp->next = nct;
 	nct->next = NULL;
 	nct->buf = buf;	
 		
---
+++ NGINX/ngx_module/handler/ngx_http_hello_module.c
@@ -0,0 +1,184 @@
+//参考rds-json-nginx-module-0.13
+
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
+
+static ngx_int_t ngx_http_hello_init(ngx_conf_t *cf);
+static void *ngx_http_hello_create_loc_conf(ngx_conf_t *cf);
+static char *ngx_http_hello_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child);
+static char * ngx_http_hello_string(ngx_conf_t *cf,ngx_command_t *cmd,void *conf);
+static ngx_int_t ngx_http_hello_handler(ngx_http_request_t *r);
+
+
+//存储location区域的配置信息
+typedef struct{ 
+	ngx_str_t hello_string;
+	ngx_str_t hello_test;
+} ngx_http_hello_loc_conf_t;
+
+//模块的指令信息
+static ngx_command_t ngx_http_hello_commands[]={
+	{
+		ngx_string("hello_string"), 
+		NGX_HTTP_LOC_CONF|NGX_CONF_NOARGS|NGX_CONF_TAKE1, //loc区域，一个或零个参数
+		ngx_http_hello_string, //解析该指令的函数,在这里可以把指令信息赋值到我们定义上面定义的结构中 
+		//用loc区域的pool分配内存,也就是解析后的数据放入哪里
+		//如果某个指令可以用在main、server配置部分，但不能用在location部分,则应该使用srv_conf存储
+		//存loc_conf中有冗余,存在man_conf中有覆盖
+		NGX_HTTP_LOC_CONF_OFFSET,	
+        //?猜测用到ngx的赋值函数如ngx_conf_set_str_slot的时候,会用到这个字段
+        offsetof(ngx_http_hello_loc_conf_t,hello_string), 
+		NULL
+	},
+	ngx_null_command
+};
+
+//模块上下文
+static ngx_http_module_t ngx_http_hello_module_ctx={
+	NULL,				 /*preconfiguration*/
+	ngx_http_hello_init, /*postconfiguration*///创建和读取该模块的配置信息之后调用
+	
+	NULL,				 /*create main configuration*/
+	NULL,				 /*init main configuration*/
+
+	NULL,				 /*create server configuration*/
+	NULL,				 /*merge server configuration*/
+
+						 //创建自定义的结构体,用来存放指令参数
+	ngx_http_hello_create_loc_conf,  /*create location configuration*/
+	NULL				 /*merge location configuration*/
+};
+
+//模块的定义
+ngx_module_t ngx_http_hello_module = {
+	NGX_MODULE_V1,			/*版本号*/
+	&ngx_http_hello_module_ctx,	/*模块上下文结构,可使用里面的一些函数*/
+	ngx_http_hello_commands,	/*模块定义的指令*/
+	NGX_HTTP_MODULE,		/*module type*/
+	NULL,				/*init master*/ master进程初始化时执行
+	NULL,				/*init module*/ master进程解析配置以后,初始化模块时调用一次
+	NULL,				/*init process*/worker进程初始化时调用一次
+	NULL,				/*init thread*/ 多线程时,线程初始化时调用,Linux下未使用
+	NULL,				/*exit thread*/ 多线程退出是调用一次
+	NULL,				/*exit process*/worker进程退出时调用一次
+	NULL,				/*exit master*/ master进程退出时调用一次
+	NGX_MODULE_V1_PADDING
+};
+
+
+//上下文中postconfiguration的函数
+static ngx_int_t ngx_http_hello_init(ngx_conf_t *cf){
+	ngx_http_handler_pt		*h;  //真正处理请求的函数
+	ngx_http_core_main_conf_t	*cmcf;
+	
+	cmcf = ngx_http_conf_get_module_main_conf(cf,ngx_http_core_module);
+    //另一种挂载方式,按需挂载
+    //cmcf = ngx_http_conf_get_module_loc_conf(cf,ngx_http_core_module);
+	//cmcf->handler = ngx_http_hello_handler;
+	//?cmcf只有一份吗
+
+	//函数注册到CONTENT阶段
+	h = ngx_array_push(&cmcf->phases[NGX_HTTP_CONTENT_PHASE].handlers);
+	if(h == NULL){
+		return NGX_ERROR;
+	}
+
+	*h = ngx_http_hello_handler;  //真正处理请求的函数
+	
+	return NGX_OK;
+}
+
+// 创建loc域的结构体
+static void *ngx_http_hello_create_loc_conf(ngx_conf_t *cf){
+	ngx_http_hello_loc_conf_t *conf = NULL;
+	conf  = ngx_pcalloc(cf->pool,sizeof(ngx_http_hello_loc_conf_t));
+	if (conf == NULL){
+		return NULL;
+	}
+
+    //初始化结构体
+	ngx_str_null(&(conf->hello_string));
+	ngx_str_set(&(conf->hello_test),"hello world test");
+
+	return conf;
+}
+
+// 合并loc域的配置信息
+static char *ngx_http_hello_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child){
+	//该方法会在ngx_http_hello_create_loc_conf和ngx_http_hello_string方法调用后在执行
+
+	ngx_http_hello_loc_conf_t *prev = parent;
+	ngx_http_hello_loc_conf_t *conf = child;
+	
+	ngx_conf_merge_str_value(conf->hello_string,prev->hello_string,"defalut");	
+
+	return NGX_CONF_OK;
+}
+
+//处理解析到的指令
+static char * ngx_http_hello_string(ngx_conf_t *cf,ngx_command_t *cmd,void *conf){
+	ngx_http_hello_loc_conf_t * loc_conf;
+	loc_conf = (ngx_http_hello_loc_conf_t *)conf;
+
+	//赋值解析到的值
+    char *rv = ngx_conf_set_str_slot(cf,cmd,loc_conf);
+	//ngx_str_set(conf->hello_string,"mashunfeng");
+	
+	return NGX_CONF_OK;
+}
+
+//真正处理请求的函数
+static ngx_int_t ngx_http_hello_handler(ngx_http_request_t *r){
+	ngx_int_t rc;
+	ngx_buf_t *b;
+	ngx_chain_t out;
+	ngx_http_hello_loc_conf_t *my_conf;
+
+	//存放从my_conf中都到的信息
+    //u_char* tmp_buf = ngx_pcalloc(r->pool, my_conf->hello_string.len + 16);// [1024] = {0};
+	u_char tmp_buf[1024] = {0};
+	ngx_uint_t content_length = 0;
+
+	//取出我们的指令配置信息
+	my_conf = ngx_http_get_module_loc_conf(r,ngx_http_hello_module);	
+	//将数据读入到临时buf中
+	ngx_sprintf(tmp_buf,"%s Hi ",my_conf->hello_string.data);
+	content_length = ngx_strlen(tmp_buf); 
+    //如果我们不需要请求体,那就把它抛弃
+	rc = ngx_http_discard_request_body(r);
+	if(rc != NGX_OK){
+		return rc;
+	}
+
+	//将数据添加到缓存链并输出
+    b = ngx_pcalloc(r->pool,sizeof(ngx_buf_t));	
+	if (b == NULL){
+		return NGX_HTTP_INTERNAL_SERVER_ERROR;
+	}
+	
+	out.buf = b;
+	out.next = NULL;
+	
+	b->pos = tmp_buf;
+	b->last = tmp_buf + content_length;
+	
+	//这个buffer在内存中,后续在write filter中用该标记判断从哪里输出数据
+	//如果是 b->file = 1 则表示后续要输出的内容在file中,如果同时设置则file起作用
+	b->memory = 1; 
+	b->last_buf = 1; //这是buffer chain中的最后一个buffer 
+
+	//需要设置响应头
+	r->headers_out.status = NGX_HTTP_OK; 
+	r->headers_out.content_length_n = content_length;
+    	
+	//需要发送响应头
+	rc = ngx_http_send_header(r);
+	if(rc == NGX_ERROR || rc > NGX_OK || r->header_only){
+		return rc;
+	}
+
+	//把buffer chain输出到response
+	return ngx_http_output_filter(r,&out);
+}
+
