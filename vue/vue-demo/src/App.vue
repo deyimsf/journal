6013c854698dd02bbc3171c920a9ccf68c5ee087
马顺风 <mashunfeng@mashunfengdeMBP.lan>
2022-07-24 22:02:59 +0800

    vue

---
+++ vue/vue-demo/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>abddd</div>
+</template>
+
+<script>
+import Web3 from 'web3'
+
+console.log(Web3);
+
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
